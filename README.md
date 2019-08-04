657fcbc511d019c6c080fde2188f3483ff2ebb8d
ozcanyarimdunya <ozcanyd@gmail.com>
2019-08-04 22:53:43 +0300

    Add discudemy and add available site list to the README.md

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ pip install requirements.txt
 cd any-folder/
 python any-file.py
 ```
+
+## Available sites
+
+- **[discudemy](https://www.discudemy.com/)**
+    - [all.py](./discudemy/all.py)
+- **[ecanlitvizle](https://www.ecanlitvizle.live/)**
+    - [tv.py](./ecanlitvizle/tv.py)
+- **[github](https://github.com/)**
+    - [topics.py](./github/topics.py)
+    - [trending.py](./github/trending.py)
+- **[sezerbozkir](https://www.sezerbozkir.com/)**
+    - [syb.py](./sezerbozkir/syb.py)
+- **[shiftdelete](https://shiftdelete.net/)**
+    - [games.py](./shiftdelete/games.py)
+    - [popular.py](./shiftdelete/popular.py)
+- **[thehackernews](https://thehackernews.com/)**
+    - [news.py](./thehackernews/news.py)
+- **[tvyayinakisi](https://www.tvyayinakisi.com/)**
+    - [rating.py](./tvyayinakisi/rating.py)
+    - [streaming.py](./tvyayinakisi/streaming.py)
---
+++ discudemy/all.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+
+from base import BaseScrapper
+
+
+class CourseScrapper(BaseScrapper):
+    base_url = 'https://www.discudemy.com'
+    url = base_url + '/all'
+    as_json = True
+    save_as_file = True
+    filename = 'all.json'
+    follow = True
+    max_result = 100
+    next_selector = 'ul.pagination3 li a'
+
+    def get_next_url(self):
+        try:
+            return self.get_soup().select(self.get_next_selector())[-1].attrs.get('href')
+        except:
+            return
+
+    def scrap(self, soup: BeautifulSoup):
+        for course in soup.select('section.card'):
+            language = course.select_one('label').get_text(strip=True)
+            if language == 'Ads':
+                return
+
+            yield {
+                'language': language,
+                'link': course.select_one('a.card-header').attrs.get('href'),
+                'title': course.select_one('a.card-header').get_text(strip=True),
+                'date': course.select_one('.meta span.category').extract().get_text(strip=True),
+                'price': course.select_one('.meta span').get_text(strip=True),
+                'image': course.select_one('.image amp-img').attrs.get('src'),
+                'description': course.select_one('.description').get_text(strip=True),
+                'category': course.select_one('.catSpan').get_text(strip=True),
+            }
+
+
+if __name__ == '__main__':
+    CourseScrapper()
